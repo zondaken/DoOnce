f52604be7ca27c2ce1cb76bd2d97e68d685221a3
ZondaKeN <marino.zirks@gmail.com>
2022-08-03 18:50:22 +0200

    Initial commit

---
+++ DoOnce/DoOnce.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+using System;
+
+public class DoOnce
+{
+    private readonly Action _action;
+    private bool _alreadyCalled = false;
+    private readonly Action? _fallback;
+
+    public DoOnce(Action action, Action? fallback = null)
+    {
+        _action = action;
+        _fallback = fallback;
+    }
+
+    public void Run()
+    {
+        if (!_alreadyCalled)
+        {
+            _action.Invoke();
+            _alreadyCalled = true;
+        }
+        else
+        {
+            _fallback?.Invoke();
+        }
+
+    }
+}
---
+++ DoOnce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var caller = new DoOnce(() => Console.WriteLine("Hello, world!"));
+        caller.Run();
+        caller.Run();
+        caller.Run();
+    }
+}
